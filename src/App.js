1ca2cc0ae7d3ad160dcaf2e07d6e0b57fb00d740
MicrotersAsad <sheblumicroters@gmail.com>
2024-03-19 15:00:14 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
 
   return (
     <div className='app'>
-      <Navbar/> {/* Pass the theme color as a prop */}
-      <hr/>
+      {/* <Navbar/> Pass the theme color as a prop */}
+      {/* <hr/> */}
       <Home/> {/* Pass the theme color as a prop */}
       {/* <div className="text-center mt-4">
         <button className="btn btn-secondary me-2" onClick={() => handleBackgroundColor('#000000')}>Black Background</button>
